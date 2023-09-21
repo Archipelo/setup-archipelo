2a890d71b674f07f4c0c5f3912086a885cd7e370
TenSt <stepan.maks@gmail.com>
2023-09-21 17:05:36 +0200

    troubleshoot test

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ async function setup() {
     // Download the specific version of the tool, e.g. as a tarball/zipball
     const download = getDownloadObject(version, qa);
     const pathToTarball = await tc.downloadTool(download.url);
-    console.log("pathToTarball", pathToTarball);
+    core.debug(pathToTarball)
 
     // Extract the tarball/zipball onto host runner
     const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
     const pathToCLI = await extract(pathToTarball);
-    console.log("pathToCLI", pathToCLI);
+    core.debug(pathToCLI)
 
     // Expose the tool by adding it to the PATH
     core.addPath(path.join(pathToCLI, download.binPath));
